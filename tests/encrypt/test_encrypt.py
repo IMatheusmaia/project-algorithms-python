307c1abb508e032d25d837252528a7e4a65c32b6
IMatheusmaia <i.matheuslmaia@gmail.com>
2024-07-31 11:59:22 -0300

    requisito 2 - teste

--- tests/encrypt/test_encrypt.py
+++ tests/encrypt/test_encrypt.py
@@ -1,5 +1,13 @@
-# from challenges.challenge_encrypt_message import encrypt_message
+import pytest
+from challenges.challenge_encrypt_message import encrypt_message
 
 
 def test_encrypt_message():
-    pass
+    with pytest.raises(TypeError):
+        encrypt_message(99, 'teste')
+        encrypt_message('teste', None)
+
+    assert encrypt_message('texto', 6) == 'otxet'
+    assert encrypt_message('texto', -2) == 'otxet'
+    assert encrypt_message("mensagem", 5) == "asnem_meg"
+    assert encrypt_message("mensagem", 4) == "mega_snem"
