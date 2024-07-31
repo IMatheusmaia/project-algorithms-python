7e3b2806ec641bca1f2cdd008ddfcd6c0c9eb0d7
IMatheusmaia <i.matheuslmaia@gmail.com>
2024-07-31 10:27:55 -0300

    correção de linter, desafio 1

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -2,10 +2,9 @@ def study_schedule(permanence_period, target_time):
     if not isinstance(target_time, (int, float)):
         return None
     online_users = 0
-    
-    for initial_time, exit_time in permanence_period:
-        if not isinstance(initial_time, (int, float)) or not isinstance(exit_time, (int, float)):
+    for initial_t, exit_t in permanence_period:
+        if not isinstance(initial_t, (int)) or not isinstance(exit_t, (int)):
             return None
-        if initial_time <= target_time <= exit_time:
+        if initial_t <= target_time <= exit_t:
             online_users += 1
     return online_users
