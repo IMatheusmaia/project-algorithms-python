786f193cba55f561cac818a3de5700b20d6f89ab
IMatheusmaia <i.matheuslmaia@gmail.com>
2024-07-31 10:11:04 -0300

    desafio 1

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,3 +1,11 @@
 def study_schedule(permanence_period, target_time):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if not isinstance(target_time, (int, float)):
+        return None
+    online_users = 0
+    
+    for initial_time, exit_time in permanence_period:
+        if not isinstance(initial_time, (int, float)) or not isinstance(exit_time, (int, float)):
+            return None
+        if initial_time <= target_time <= exit_time:
+            online_users += 1
+    return online_users
